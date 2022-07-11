820f1c5c9efe540b5149234abe2bb75cd9053a17
Sadatsafavi <mohsen.sadatsafavi@ubc.ca>
2022-07-11 16:27:02 -0700

    Updates for Harry

--- R/perturbation_sim.R
+++ R/perturbation_sim.R
@@ -51,6 +51,8 @@ perturbed_scenario <- function(sample_size,  true_model = c(-1.55,0.77), event_p
     pi <- as.vector(1/(1+exp(-log_lins)))
   }
   
+  predprev <- mean(pi)
+  
   auc <- roc(Y, pi, quiet=T)$auc
   #message("AUC is", auc)
   
@@ -59,10 +61,13 @@ perturbed_scenario <- function(sample_size,  true_model = c(-1.55,0.77), event_p
   
   check <- unname(coefficients(glm(Y~logit_pi, data=sample, family=binomial())))
   
-  res <- voi_ex_glm(model=NULL, val_data=sample, method = "bootstrap", zs = zs, weights = weights)
-  res2 <- dca(sample, zs=zs)
+  res_ob <- voi_ex_glm(model=NULL, val_data=sample, method = "bootstrap", zs = zs, weights = weights)
+  res_bb <- voi_ex_glm(model=NULL, val_data=sample, method = "bootstrap", Bayesian_bootstrap = T, zs = zs, weights = weights)
+  res_as <- voi_ex_glm(model=NULL, val_data=sample, method = "asymptotic", zs = zs, weights = weights)
   
-  c(EVPIv=res[,'EVPIv'], NB_model=res2[,'NB_model'], NB_all=res2[,'NB_all'],  auc=auc, prev=prev, check_intercept=check[1], check_slope=check[2])
+  res_dca <- dca(sample, zs=zs)
+  
+  c(EVPIv_ob=res_ob[,'EVPIv'], EVPIv_bb=res_bb[,'EVPIv'], EVPIv_as=res_as[,'EVPIv'], NB_model=res_dca[,'NB_model'], NB_all=res_dca[,'NB_all'], p_useful=res_ob$p_useful, auc=auc, prev=prev, predprev=predprev, check_intercept=check[1], check_slope=check[2])
 }
 
 
@@ -71,8 +76,8 @@ perturbed_scenario <- function(sample_size,  true_model = c(-1.55,0.77), event_p
 perturbed_scenarios <- function(sample_sizes = c(125, 250, 500, 1000),
                                  event_ps = NULL,
                                  noise_sds = c(0,0.15,0.30,0.45,0.60),
-                                 c_intercepts = c(-0.8,-0.4,0,0.4,0.8),
-                                 zs = c(0.1,0.2,0.3),
+                                 c_intercepts = c(-3.2, -1.6, -0.8, -0.4, 0, 0.4, 0.8, 1.6, 3.2),
+                                 zs = c(0.1,0.2,0.3), #seq(0.25,0.75,length=21),
                                  seed=NULL)
 {
   out <- NULL #data.frame("sample_size"=integer(),"event_p"=double(), "noise_sd"=double(),"c_intercept"=double(),"voi_1"=double(), "voi_2"=double(),"voi_3"=double(),"voi_4"=double(),  "auc"=double())
@@ -132,11 +137,15 @@ main <- function(n_sim=10)
 process_results <- function()
 {
   x<-sqldf("SELECT COUNT(*) AS N, sample_size, event_p, noise_sd, c_intercept, 
-        AVG(EVPIv1) AS EVPIv1, AVG(EVPIv2) AS EVPIv2, AVG(EVPIv3) AS EVPIv3, 
-        SQRT(VARIANCE(EVPIv1)/count(*)) AS se1, SQRT(VARIANCE(EVPIv2)/count(*)) AS se2, SQRT(VARIANCE(EVPIv3)/count(*)) AS se3, 
+        AVG(evpiv_ob1) AS EVPIv_ob1, AVG(evpiv_ob2) AS EVPIv_ob2, AVG(evpiv_ob3) AS EVPIv_ob3,
+        AVG(evpiv_bb1) AS EVPIv_bb1, AVG(evpiv_bb2) AS EVPIv_bb2, AVG(evpiv_bb3) AS EVPIv_bb3,
+        AVG(evpiv_as1) AS EVPIv_as1, AVG(evpiv_as2) AS EVPIv_as2, AVG(evpiv_as3) AS EVPIv_as3,
+        SQRT(VARIANCE(evpiv_ob1)/count(*)) AS se1, SQRT(VARIANCE(evpiv_ob1)/count(*)) AS se2, SQRT(VARIANCE(evpiv_ob1)/count(*)) AS se3, 
+        AVG(p_useful1) AS p_useful1, AVG(p_useful2) AS p_useful2, AVG(p_useful3) AS p_useful3, 
+        SQRT(VARIANCE(NB_model1-MAX(NB_all1,0))) AS sddNB1, SQRT(VARIANCE(NB_model2-MAX(NB_all2,0))) AS sddNB2, SQRT(VARIANCE(NB_model3-MAX(NB_all3,0))) AS sddNB3, 
         AVG(NB_model1) AS NB_model1, AVG(NB_model2) AS NB_model2, AVG(NB_model3) AS NB_model3, 
         AVG(NB_all1) AS NB_all1, AVG(NB_all2) AS NB_all2, AVG(NB_all3) AS NB_all3, 
-        AVG(auc) AS auc, AVG(prev) AS prev,
+        AVG(auc) AS auc, AVG(prev) AS prev, AVG(predprev) AS predprev, 
         AVG(check_intercept) AS check_intercept, AVG(check_slope) AS check_slope
         FROM res GROUP BY sample_size, event_p, noise_sd, c_intercept")
   
