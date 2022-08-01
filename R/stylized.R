63b1dc2ee569b2325dbd34214bc657b16930df6b
Tae Yoon (Harry) Lee <taeyoon.harry.lee@gmail.com>
2022-08-01 14:25:36 -0700

    update the stylized example

--- R/stylized.R
+++ R/stylized.R
@@ -1,4 +1,6 @@
 library(MASS)
+remotes::install_github("resplab/predtools")
+library("predtools")
 data(birthwt)
 
 n <- dim(birthwt)[1]
@@ -6,12 +8,18 @@ z <- 0.2 #This is the risk threshold
 
 #Our model is logit(pi) = 2 -0.05*age -0.01*lwt
 
+# set seed for reproducibility
+set.seed(2022)
+
 #Step 1: calculate predicted probabilities
 birthwt$pi <- 1/(1+exp(-(2-0.05*birthwt$age-0.01*birthwt$lwt)))  #Predicted risks
 
+# Bootstrap method
+
 #Step 2: The bootstrap Monte Carlo simulation
-NBmodel <- NBall <- NBmax <- rep(0,1000)
-for(i in 1:1000) 
+N <- 10000
+NBmodel <- NBall <- NBmax <- rep(0,N)
+for(i in 1:N) 
 {
   bsdata <-  birthwt[sample(1:n, n, replace = T),] 
   NBall[i] <- mean(bsdata$low-(1-bsdata$low)*z/(1-z)) 
@@ -20,13 +28,13 @@ for(i in 1:1000)
 
 #Step 3: EVPI calculation
 EVPI <- mean(pmax(0,NBmodel,NBall))-max(0,mean(NBmodel),mean(NBall))
+# 0.0008698413
 
-
-
+# Asymptotic method
 
 #Step 2: Estimate the moments
 Y <- birthwt$low; pi <- birthwt$pi; a <- I(pi>z); rho <- mean(Y); TPR <- mean(Y*a)/rho; FPR <- mean(a*(1-Y)/(1-rho)); tz <- z/(1-z)
-NBmodel <- mean(Y-(1-Y)*tz)
+NBmodel <- mean(a*(Y-(1-Y)*tz))
 NBall <- mean(Y-(1-Y)*tz)
 
 Sigma <- matrix(0,nrow=2,ncol=2)
@@ -44,3 +52,4 @@ A <- mu_max_trunc_bvn(c(NBmodel,NBall),Sigma)
 
 # Step 4: EVPI calculation
 EVPI <- A - max(0,NBmodel,NBall)
+# 0.0007555751
